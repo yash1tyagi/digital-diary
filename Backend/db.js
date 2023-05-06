a98aa152801056cec047276611da0183f2dc2251
yash1tyagi <94501085+yash1tyagi@users.noreply.github.com>
2023-05-06 22:05:13 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,24 +1,20 @@
 const { MongoClient } = require('mongodb');
 const mongoose = require('mongoose');
 require('dotenv').config();
-const mongoURI =  process.env.MONGO_URI
-// const mongoURI = "mongodb://localhost:27017"
 
 const connectToMongo = async ()=>{
-mongoose.connect(mongoURI, ()=>{
-console.log("Connect to mongo ");
-})
+    try {
+      const conn = await mongoose.connect(process.env.mongo_uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      //   useFindAndModify: true,
+      });
+  
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(`Error: ${error.message}`);
+      process.exit();
+    }
 }
 module.exports = connectToMongo;
 
-// atles - mongodb+srv://yash:<y1ash2tyagi>@cluster0.jatas.mongodb.net/mern?retryWrites=true&w=majority 
- 
-// compus - mongodb://localhost:27017/yash?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false
-
-
-// {
-//     // useNewUrlParser: true,
-//    //  UseCreateIndex:true,
-//      //useUnifiedTopology:true,
-//      //useFindAndModify:false
-//  },
